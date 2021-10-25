04439078a1e355fbd8a5ef89f982bb6c24ed47c6
jo <a0750318@gmail.com>
2021-10-25 17:50:30 +0800

    add index.handlebars,mongoose config,edit app.js

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const exphbs = require('express-handlebars')
+const app = express()
+const routes = require('./routes')
+const port = 3000
+const methodOverride = require('method-override')
+
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config()
+}
+
+//資料庫連線
+require('./config/mongoose')
+
+
+//使用layout既定格式
+app.engine('handlebars', exphbs({defaultLayout: 'main'}))
+app.set('view engine', 'handlebars')
+
+// CSS
+app.use(express.static('public'))
+
+app.use(methodOverride('_method'))
+
+app.use(express.urlencoded({extended: true}))
+
+app.use(routes)
+
+// start and listen on the Express server
+app.listen(port, () => {
+    console.log(`Express is listening on localhost:${port}`)
+})
